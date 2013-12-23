2f243cc1da79ee997b2f965bff78353ad45b94c6
pmacnaughton <peter@macnaughton.info>
2013-12-23 10:02:52 -0700

    added custom message overload for GuidArgumentExtensions.IsNotEmpty

--- src/RequireThat/GuidArgumentExtensions.cs
+++ src/RequireThat/GuidArgumentExtensions.cs
@@ -14,12 +14,18 @@ namespace RequireThat
         /// <returns>The <seealso cref="RequireThat.Argument"/> that the extension was called on.</returns>
         /// <exception cref="ArgumentException">Thrown if the requirement is not met.</exception>
         [DebuggerStepThrough]
-        public static Argument<Guid> IsNotEmpty(this Argument<Guid> statement)
+        public static Argument<Guid> IsNotEmpty(this Argument<Guid> argument)
         {
-            if (Guid.Empty.Equals(statement.Value))
-                throw ExceptionFactory.CreateArgumentException(statement, ExceptionMessages.EmptyGuid);
+            return argument.IsNotEmpty(ExceptionMessages.EmptyGuid);
+        }
 
-            return statement;
+        [DebuggerStepThrough]
+        public static Argument<Guid> IsNotEmpty(this Argument<Guid> argument, string message)
+        {
+            if (Guid.Empty.Equals(argument.Value))
+                throw ExceptionFactory.CreateArgumentException(argument, message);
+
+            return argument;
         }
     }
 }
